03d2b0638f93f0f01bee8dd47a3e1c13a12e16f3
laderashielamae <laderaxiexy@gmail.com>
2025-04-09 17:35:53 +0800

    Add files via upload

---
+++ week5.java
@@ -0,0 +1,50 @@
+public class Week5 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+
+        // Call each student's method in alphabetical order
+        AgpoonGregpsalms.display();
+        AgpoonGregsamuel.display();
+        Alabado.display();
+        Amar.display();
+        Arevalo.display();
+        Artiaga.display();
+        Balais.display();
+        Banaag.display();
+        Bendoy.display();
+        Berte.display();
+        Borja.display();
+        Credo.display();
+        Costanilla.display();
+        DeJesus.display();
+        Dichoso.display();
+        Dizon.display();
+        Echon.display();
+        Eugenio.display();
+        Flores.display();
+        Garcia.display();
+        Gabronino.display();
+        Jesalva.display();
+        Lamac.display();
+        Mago.display();
+        Mangui.display();
+        Mata.display();
+        Miranda.display();
+        Pajanustan.display();
+        Panis.display();
+        Pascualado.display();
+        Pianar.display();
+        Regoroso.display();
+        Respondo.display();
+        Ringor.display();
+        Saulong.display();
+        Sayas.display();
+        Serrano.display();
+        Tabon.display();
+        Talidong.display();
+        Tapia.display();
+        Tado.display();
+        Villaos.display();
+        Villon.display();
+    }
+}
